d7b04d2c6c572997803f8b0c8984a5f80582b74e
Jimmyelvis <jinjoe@zoho.com>
2022-08-01 19:46:07 -0400

    changes to deploy

---
+++ FrontPageTestData.js
@@ -0,0 +1,171 @@
+ const articles = [
+   {
+     tags: ["AIC", "Hockey"],
+     categories: [
+       {
+         _id: "5f7ba80ff98efee1d0650deb",
+         name: "Sports",
+         slug: "sports",
+       },
+     ],
+     _id: "6175b522497663711c9262c1",
+     title: "Sample Entry 1",
+     subtitle: "This is a sample place holder entry.",
+     body: "<p>In est arcu, posuere id tempus ut, mollis ac dui. Praesent non dictum erat. Ut imperdiet, est eu mattis sagittis, lectus neque luctus eros, eu vehicula ex felis sit amet orci. In placerat posuere eleifend. Aliquam rhoncus elit nec rutrum laoreet. Integer cursus eu quam vitae aliquet. Nunc ut neque et elit tempor bibendum pulvinar vel justo.</p><p><br></p><p>Mauris nec malesuada turpis. Mauris blandit nec neque ac laoreet. Proin nec sapien felis. Proin quis tortor luctus leo eleifend mollis. Cras eu nulla ac mi laoreet feugiat. Etiam vitae nunc risus. Morbi elementum mi sit amet velit maximus condimentum. Nulla bibendum vestibulum dolor eu rhoncus. Maecenas dapibus arcu id bibendum egestas. Donec varius consectetur magna tristique pretium. Phasellus ex dui, ultrices ut diam sit amet, posuere dapibus arcu. Sed vestibulum sodales neque et gravida. Nam cursus lectus id nunc posuere finibus.</p><p><br></p><p>Curabitur rhoncus faucibus odio, ac aliquet lacus gravida vel. Vestibulum sit amet neque orci. Vivamus porttitor diam sapien, mollis porta urna venenatis eu. Duis venenatis id erat in ultrices. Mauris turpis elit, blandit a rhoncus eu, ultricies quis lectus. Fusce nec euismod arcu. Morbi eget turpis enim. Mauris porta luctus facilisis. Fusce tempor ante eu ipsum euismod, sed consectetur nibh pharetra. Sed volutpat erat sit amet dolor hendrerit, sit amet vehicula odio molestie. Vestibulum sapien diam, hendrerit eget dictum luctus, accumsan sit amet nunc. Fusce feugiat lacus vel eleifend bibendum. Aliquam ultrices efficitur tristique.</p><p><br></p><p>Quisque sed ex et justo convallis faucibus vel vel dolor. Mauris vehicula, lorem eu efficitur blandit, neque tellus gravida odio, ac feugiat turpis tortor id ex. Pellentesque quis tempor sapien. Morbi pharetra urna nec lectus posuere tempor. Pellentesque varius ante et vestibulum ultricies. Sed suscipit ex interdum dapibus varius. Duis ut purus eget erat facilisis tincidunt.</p><p><br></p><p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum cursus bibendum ullamcorper. Fusce non sem non nisi blandit gravida sit amet ac libero. Sed venenatis vel purus ac eleifend. Pellentesque in scelerisque massa. Curabitur commodo commodo odio, ac congue magna mattis in. Nulla vehicula euismod vestibulum. Sed quis placerat turpis. Phasellus volutpat mi ligula, vitae commodo magna eleifend non. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam faucibus dapibus ante sit amet scelerisque. Proin tempus pretium viverra. Donec pulvinar volutpat pretium. Morbi id tempor libero. Suspendisse potenti.</p><p><br></p><p>Donec pulvinar id tortor vel mattis. Nulla tempus varius consectetur. Suspendisse potenti. Donec ut massa rhoncus, lacinia ex a, luctus lorem. Donec venenatis elementum tortor, eu dignissim ante dictum id. Mauris convallis augue et nisi lacinia, a vulputate elit consequat. Vestibulum elementum blandit sapien. Nam rutrum erat scelerisque, placerat tortor feugiat, faucibus purus.</p><p><br></p><p>Cras feugiat blandit vulputate. Integer sapien nulla, posuere ut laoreet sit amet, consectetur nec est. Cras eu sodales purus. Fusce at quam a tellus maximus vestibulum. Suspendisse finibus maximus dictum. Aliquam quis ipsum iaculis, gravida risus eget, pellentesque arcu. Vivamus sed egestas turpis. Aliquam magna erat, cursus vel est a, ornare varius dolor. Sed eu justo luctus, ornare libero in, auctor tellus. Integer ligula lacus, pretium eget urna sed, condimentum commodo dolor. Aliquam semper facilisis ultricies. Vivamus tempus sem eu tortor hendrerit dapibus. Cras sollicitudin tincidunt augue, vitae varius risus mollis ut.</p>",
+     mainphoto:
+       "https://res.cloudinary.com/dwgjvssdt/image/upload/v1638050894/upfhkvlbszhmsbmzzmmk.jpg",
+     slug: "sample-entry",
+     mtitle: "AIC men's hockey team falls short in home opener | MassNow",
+     mdesc:
+       "In est arcu, posuere id tempus ut, mollis ac dui. Praesent non dictum erat. Ut imperdiet, est eu mattis sagittis, lectus neque luctus eros, eu vehicula ex fe",
+     postedBy: {
+       _id: "61620db37e93c5254822150a",
+       name: "Jordy Carter",
+       username: "jordy",
+       photo:
+         "http://res.cloudinary.com/dwgjvssdt/image/upload/v1634405691/axi2ilcyja4u35kkh4dd.jpg",
+     },
+     featuredTopstory: 1,
+     featuredSports: 0,
+     createdAt: "2021-10-24T19:33:54.447Z",
+     updatedAt: "2021-10-24T19:33:54.492Z",
+     featuredLocal: 0,
+   },
+   {
+     tags: ["High School Sports", "Covid Restrictions"],
+     categories: [
+       {
+         _id: "5f7ba80ff98efee1d0650deb",
+         name: "Sports",
+         slug: "sports",
+       },
+       {
+         _id: "5f7ba819f98efee1d0650dec",
+         name: "Local News",
+         slug: "local-news",
+       },
+     ],
+     _id: "616a51df25540733e8b5db6f",
+     title: "Sample Entry 2",
+     subtitle: "High School Sports Still Go On",
+     mainphoto:
+       "https://res.cloudinary.com/dwgjvssdt/image/upload/v1634404087/op6mf9uthxohucexpdlr.jpg",
+     excerpt:
+       "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vehicula leo elit, sed egestas tortor porta ac. Praesent eget mi at orci posuere mollis at et est. Nullam sagittis leo venenatis elementum consectetur. Sed mollis, magna ac dictum suscipit, justo leo hendrerit est, pellentesque mattis risus odio ac turpis. ...",
+     slug: "despite-covid-restrictions-high-school-sports-still-go-on",
+     mdesc:
+       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vehicula leo elit, sed egestas tortor porta ac. Praesent eget mi at orci posuere mollis at et est",
+     postedBy: {
+       _id: "61620e347e93c5254822150b",
+       name: "Mike Vincent",
+       username: "vincent",
+     },
+     createdAt: "2021-10-16T04:15:27.533Z",
+     updatedAt: "2022-04-23T21:24:37.566Z",
+     featuredTopstory: 2,
+     featuredSports: 0,
+     featuredLocal: 0,
+   },
+   {
+     tags: ["Big E", "County Fair"],
+     categories: [
+       {
+         _id: "5f7ba819f98efee1d0650dec",
+         name: "Local News",
+         slug: "local-news",
+       },
+     ],
+     _id: "616a42a425540733e8b5db6a",
+     title: "Sample Entry 3",
+     subtitle: "The Big E is billed as the third largest fair in the country ",
+     mainphoto:
+       "https://res.cloudinary.com/dwgjvssdt/image/upload/v1634403291/gf7m3xvwmhfa1knqgxfo.jpg",
+     excerpt:
+       "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat ac tincidunt vitae semper. Pulvinar mattis nunc sed blandit libero volutpat. Risus pretium quam vulputate dignissim suspendisse. Semper feugiat nibh sed pulvinar proin gravida ...",
+     slug: "2021-big-e-fair-draws-big-crowds",
+     mdesc:
+       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Volutpat ac tincidunt vitae sempe",
+     postedBy: {
+       _id: "5f7a39ab7acc30a29c5280df",
+       name: "Dylan Cougar",
+       username: "dylancougar",
+     },
+     createdAt: "2021-10-16T03:10:28.039Z",
+     updatedAt: "2022-04-24T23:39:52.830Z",
+     featuredTopstory: 3,
+     featuredSports: 0,
+     featuredLocal: 0,
+   },
+   {
+     tags: ["Music Festival", "Bands"],
+     categories: [
+       {
+         _id: "5f7ba819f98efee1d0650dec",
+         name: "Local News",
+         slug: "local-news",
+       },
+       {
+         _id: "5f7a5c123fcb52c9ac96ec00",
+         name: "Latest News",
+         slug: "latest-news",
+       },
+     ],
+     _id: "616a490425540733e8b5db6c",
+     title: "Local Music Festival draws Thousands",
+     subtitle: "Lorem ipsum dolor sit amet",
+     mainphoto:
+       "http://res.cloudinary.com/dwgjvssdt/image/upload/v1634355368/nli98udgkbhh5lkg9h1h.jpg",
+     excerpt:
+       "<p>Non pinterest hexagon, man braid tilde twee est intelligentsia. Hell of sriracha lomo heirloom velit asymmetrical cardigan VHS kogi. Taxidermy messenger bag sed, slow-carb migas swag neutra single-origin coffee. Sriracha austin sustainable slow-carb dolore anim enamel pin.</p><p><br></p><p>Gochujang kale chips ...",
+     slug: "local-music-festival-draws-thousands",
+     mdesc:
+       "Non pinterest hexagon, man braid tilde twee est intelligentsia. Hell of sriracha lomo heirloom velit asymmetrical cardigan VHS kogi. Taxidermy messenger bag",
+     postedBy: {
+       _id: "615392d29eda08932c92de09",
+       name: "Kate Vanny",
+       username: "vanny",
+     },
+     createdAt: "2021-10-16T03:37:40.931Z",
+     updatedAt: "2022-04-24T23:40:33.262Z",
+     featuredTopstory: 4,
+     featuredSports: 0,
+     featuredLocal: 0,
+   },
+   {
+     tags: ["Downtown", "Concert Series", "Music"],
+     categories: [
+       {
+         _id: "5f7ba819f98efee1d0650dec",
+         name: "Local News",
+         slug: "local-news",
+       },
+       {
+         _id: "5f7a5c123fcb52c9ac96ec00",
+         name: "Latest News",
+         slug: "latest-news",
+       },
+     ],
+     _id: "616af5f144cbc64c5c8048dc",
+     title: "Downtown Concert Series Set To  Start This Week",
+     subtitle: "Pulvinar mattis nunc sed blandit libero volutpat",
+     mainphoto:
+       "http://res.cloudinary.com/dwgjvssdt/image/upload/v1634399516/vqukzqwmb1tphalilqd5.jpg",
+     excerpt:
+       "<p>Mauris a erat maximus, sollicitudin leo sit amet, gravida orci. Donec condimentum, nisi et pulvinar aliquet, purus nunc vestibulum nisi, vel tristique risus ligula eget dui. Nulla eros felis, posuere sed pellentesque sed, molestie ut sem. Nunc ac dolor vitae eros elementum vulputate vel sed justo. Sed nisl massa, ...",
+     slug: "downtown-concert-series-set-to-start-this-weekend",
+     mdesc:
+       "Mauris a erat maximus, sollicitudin leo sit amet, gravida orci. Donec condimentum, nisi et pulvinar aliquet, purus nunc vestibulum nisi, vel tristique risus",
+     postedBy: {
+       _id: "61621e0c7e93c5254822150c",
+       name: "Marcia Chess",
+       username: "mashchess",
+     },
+     createdAt: "2021-10-16T15:55:29.178Z",
+     updatedAt: "2022-04-22T20:33:11.707Z",
+     featuredTopstory: 5,
+     featuredSports: 0,
+     featuredLocal: 3,
+   },
+ ];
+
+ export default articles;
